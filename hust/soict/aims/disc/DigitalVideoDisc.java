31bc769d339e188125721d243fecaa5b26563703
DoTienDat-20225700 <caubengungo1611@gmail.com>
2024-11-26 01:51:10 +0700

    refractor

--- hust/soict/aims/disc/DigitalVideoDisc.java
+++ hust/soict/aims/disc/DigitalVideoDisc.java
@@ -1,3 +1,4 @@
+package hust.soict.aims.disc;
 
 public class DigitalVideoDisc {
 	private String title;
@@ -5,6 +6,8 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
+	private int id;
+	private static int nbDigitalVideoDiscs = 0;
 	
 	//Getters
 	public String getTitle() {
@@ -20,13 +23,20 @@ public class DigitalVideoDisc {
 		return length;
 	}
 	public float getCost() {
-		return cost;
+		return cost;	
 	}
+	public int getId() { 
+		return id;
+	}
+	public static int getNbDigitalVideoDiscs() { 
+        return nbDigitalVideoDiscs;
+    }
 	
 	// Constructors
     public DigitalVideoDisc(String title) {
        super();
        this.title = title;
+       incrementAndSetId(); 
     }
 
 
@@ -35,6 +45,7 @@ public class DigitalVideoDisc {
        this.title = title;
        this.category = category;
        this.cost = cost;
+       incrementAndSetId(); 
    }
 
    public DigitalVideoDisc(String director, String category, String title, float cost){
@@ -43,6 +54,7 @@ public class DigitalVideoDisc {
        this.category = category;
        this.cost = cost;
        this.director = director;
+       incrementAndSetId(); 
    }
 
    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -52,12 +64,29 @@ public class DigitalVideoDisc {
        this.director = director;
        this.length = length;
        this.cost = cost;
+       incrementAndSetId(); 
+   }
+   // Private method to increment the number of DVDs and set the unique ID
+   private void incrementAndSetId() {
+       nbDigitalVideoDiscs++;
+       this.id = nbDigitalVideoDiscs;
    }
    //toString method to display DVD 
-@Override
-public String toString() {
-	return "DigitalVideoDisc [title=" + title + ", category=" + category + ", director=" + director + ", length="
-			+ length + ", cost=" + cost + "]";
+   @Override
+	public String toString() {
+		return "DigitalVideoDisc [id=" + id + ", title=" + title + ", category=" + category + ", director=" + director + ", length="
+				+ length + ", cost=" + cost + "]";
+	}
+
+   //Setter for title
+	public void setTitle(String title) {
+    if (title != null && !title.trim().isEmpty()) {
+        	this.title = title;
+    	} else {
+    		System.out.println("Invalid title. Title cannot be null or empty.");
+    	}
+	}
 }
 
-}
+
+
